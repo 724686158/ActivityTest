9d099dfe2a2d17a44d1f63b91acb0f8c6d0e23cf
mengzicheng <724686158@qq.com>
2016-09-13 13:53:13 +0800

    新增Menu

--- app/src/main/java/com/example/admin/activitytest/FirstActivity.java
+++ app/src/main/java/com/example/admin/activitytest/FirstActivity.java
@@ -2,7 +2,10 @@ package com.example.admin.activitytest;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.Toast;
 
@@ -23,5 +26,27 @@ public class FirstActivity extends Activity {
                 Toast.makeText(FirstActivity.this, "YOU CLICKED BUTTON 1", Toast.LENGTH_SHORT).show();
             }
         });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId())
+        {
+            case R.id.add_item:
+                Toast.makeText(this, "YOU CLICKED ADD", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.remove_item:
+                Toast.makeText(this, "YOU CLICKED REMOVE", Toast.LENGTH_SHORT).show();
+                break;
+            default:
+                break;
+        }
+        return true;
     }
 }
